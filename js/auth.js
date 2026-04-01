f3c98594c25d9c7cedefbabea0f2b927cf5d30b3
ahsan <ahsanlasiqa@gmail.com>
2026-04-01 13:56:50 +0700

    prod6

--- js/auth.js
+++ js/auth.js
@@ -200,7 +200,7 @@ function startAuth() {
 
 // Hook landing page buttons after DOM is ready
 function hookLandingButtons() {
-  ['btn-coba-gratis', 'btn-coba-free', 'btn-google-nav', 'btn-coba-pain'].forEach(function(id) {
+  ['btn-coba-gratis', 'btn-coba-free', 'btn-google-nav', 'btn-coba-pain', 'btn-coba-gratis-cpns'].forEach(function(id) {
     var btn = document.getElementById(id);
     if (btn) btn.addEventListener('click', function() {
       document.getElementById('btn-google-login').click();
